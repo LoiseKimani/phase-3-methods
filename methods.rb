548af4900ab77d1ceff1e4ac0160d934e772c142
Loise <loise.kimani@student.moringaschool.com>
2023-02-07 01:32:27 +0300

    Done

--- methods.rb
+++ methods.rb
@@ -1,1 +1,30 @@
-# Your code here!
+# Your code here
+
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+  
+    greet("Naureen")
+
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+
+end 
+greet_with_default
+  
+  def add(num1, num2)
+    return num1 + num2
+ end
+ 
+  add(2,5)
+
+  def halve(num)
+    return nil unless num.is_a?(Integer)
+    n.div(2)
+  end
+  
